ddedc2a6eac940e2c3bb6ca83019e3159f2fef5a
Minseokang <minseo_0606@naver.com>
2021-03-31 12:13:33 +0900

    (feat):Add median

--- README.md
+++ README.md
@@ -3,16 +3,33 @@
 ## 프로젝트 동기
 2021년 3월 31일 수학 3-2 문제를 푸는 강민서는 이 지긋지긋한 계산에 빡이 쳤다. 그래서 그냥 이걸 계산하는 라이브러리를 만들고자 이 프로젝트를 시작하게 되었다.
 
-## 작동방법
+## 모듈 임포트하기 
 ``` python
 >>> from Statistical_math.Calculate import Calculate
 
 >>> cal = Calculate()
->>> # 평균 구하기
+```
+
+
+# 평균 구하기
+``` python
 >>> print(cal.mean([1,2,3,4,5,6]))
 3.5
-
->>> # 편차 구하기
->>> print(cal.dispersion([1,2,3,4,5,6]))
+```
+# 중앙값 구하기
+``` python
+>>> print(cal.median([4, 3, 2, 3, 4, 5, 6]))
+3
+```
+# 편차 구하기
+```python 
+>>> print(cal.deviation([1,2,3,4,5,6]))
 [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]
+```
+
+# 분산 구하기
+```python 
+
+>>> print(cal.dispersion([1,2,3,4,5,6]))
+2.9166666666666665
 ```
--- Statistical_math/Calculate.py
+++ Statistical_math/Calculate.py
@@ -12,7 +12,7 @@ class Calculate:
             meanloop += int(l)
             meanloopadd += 1
         return meanloop / meanloopadd
-    def dispersion(self, ls:list):
+    def deviation(self, ls:list):
         meanloop = 0
         meanloopadd = 0
         for l in ls:
@@ -36,12 +36,20 @@ class Calculate:
         else:
             return init[0]
 
-
-
-
-
-
-
+    def dispersion(self, ls:list):
+        meanloop = 0
+        meanloopadd = 0
+        outputint = 0
+        for l in ls:
+            meanloop += int(l)
+            meanloopadd += 1
+        mean = meanloop / meanloopadd
+        output = []
+        for j in ls:
+            output.append(int(j) - mean)
+        for k in output:
+            outputint += (k ** 2)
+        return outputint / len(ls)
 
 
 if __name__ == '__main__':
