4c9d345d4b912442d2f7aae33131e83f55ef8ec9
Minseokang <minseo_0606@naver.com>
2021-04-03 01:00:09 +0900

    (feat) : Add GUI entry and skeleton

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-from Statistical_math.Calculate import Calculate
+#======================
+# imports
+#======================
+import tkinter as tk
+from tkinter import ttk
 
-cal = Calculate()
-# 평균 구하기
-print(cal.mean([1,2,3,4,5,6]))
-print(cal.deviation([1,2,3,4,5,6]))
-print(cal.median([4, 3, 2, 3, 4, 5, 6]))
-print(cal.dispersion([1,2,3,4,5,6]))
+# Create instance
+win = tk.Tk()   
+
+# Add a title       
+win.title("Python GUI")
+win.geometry("640x480")
+win.resizable(False, False)
+# Modify adding a Label
+a_label = ttk.Label(win, text="주어진 데이터 세트를 입력하세요 :")
+a_label.grid(column=0, row=0)
+
+# Modified Button Click Function
+def click_me():
+    global dataset
+    dataset = name.get()
+    print(dataset)
+
+
+# Adding a Text box Entry widget
+name = tk.StringVar()
+name_entered = ttk.Entry(win, width=12, textvariable=name)
+name_entered.grid(column=1, row=0)
+
+# Adding a Button
+action = ttk.Button(win, text="입력", command=click_me)
+action.grid(column=2, row=0)
+
+#======================
+# Start GUI
+#======================
+win.mainloop()
